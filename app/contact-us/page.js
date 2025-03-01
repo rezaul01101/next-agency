b063fc5018c0501800b7283f3ec119b57b286763
Rezaul Hoque <rezaul01101@gmail.com>
2025-03-01 11:27:16 +0600

    Map add in contact page

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -83,6 +83,7 @@ const ContactUs = () => {
           </div>
         </div>
       </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158858.58518384286!2d-0.26640253299975086!3d51.52852620463015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sbd!4v1740806515167!5m2!1sen!2sbd" width="100%" height="350" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </>
   );
 };
